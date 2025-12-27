c05af94a5eaa2d0c9c46c775ffcbf56c9ea77e15
StudyHTMLCSS <121604080+StudyHTMLCSS@users.noreply.github.com>
2025-12-27 23:37:07 +0300

    Update vite.config.ts
    Signed-off-by: StudyHTMLCSS <121604080+StudyHTMLCSS@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Englishcours/', // 👈 ОБЯЗАТЕЛЬНО для GitHub Pages
+  base: '/Englishcourse/', // 👈 ОБЯЗАТЕЛЬНО для GitHub Pages
   plugins: [
     react(),
     tailwindcss(),
